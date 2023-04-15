fd853b7d620ab23277bea83dddb4953230454f65
Carlos Norambuena <cnoramb1@my.centennialcollege.ca>
2023-04-15 11:23:47 -0400

    Initial Commit

---
+++ User Account/User_AccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_AccountApp.swift
+//  User Account
+//
+//  Created by Carlos Norambuena on 2023-04-15.
+//
+
+import SwiftUI
+
+@main
+struct User_AccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
